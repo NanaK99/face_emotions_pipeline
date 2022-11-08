ebc1ab67a548017a63f68e27ad3fc412698fd65b
NanaK99 <nana@themagicallabs.com>
2022-11-09 00:12:49 +0400

    slightly changed face_pipeline for short demo video

--- face_pipeline_textgrid.py
+++ face_pipeline_textgrid.py
@@ -5,132 +5,56 @@ import cv2
 from collections import Counter
 
 
-new_data = ['File type = "ooTextFile"\n', 'Object class = "TextGrid"\n', '\n',
-            'xmin = 0\n', 'xmax = 10175.872\n', 'tiers? <exists>\n',
-            'size = 5\n', '    class = "IntervalTier"\n',
-            '    xmin = 0\n', '    xmax = 10175.872\n', '    intervals: size = 3391\n']
+CAMERA = 0
+cap = cv2.VideoCapture(CAMERA)
 
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH) + 0.5)
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT) + 0.5)
+size = (width, height)
+fourcc = cv2.VideoWriter_fourcc(*'XVID') #*'MJPG'
+out = cv2.VideoWriter('output.avi', fourcc, 20.0, size)
 
-video_path = "vid.mp4"
-cap = cv2.VideoCapture(video_path)
-frame_count = cap.get(cv2.CAP_PROP_FRAME_COUNT)
-fps = cap.get(cv2.CAP_PROP_FPS)
-print(f"Frames per second is equal to {fps}.")
-duration_sec = frame_count/fps
-duration_min = duration_sec/60
-print(f"Duration of {video_path} is {duration_min:.2f} minutes.")
-
-interval_sec = 3
-number_of_frames_in_interval = fps * interval_sec
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 
-gaze_text = []
-mediapipe_text = []
-faceexpr_text = []
-
-head_shake_dir = []
-head_nod_dir = []
-head_shake_idxs = []
-head_nod_idxs = []
-
-frame_idx = 0
-interval_idx = 1
-interval_size = 3391
-
-x_min = 0
-x_max = interval_sec
+gaze_text = ""
+faceexpr_text = ""
 
 while cap.isOpened():
+    success, img = cap.read()
+    if not success: break
 
-    if interval_idx <= 5:
-        print(f"Processing {interval_idx} interval")
-        if frame_idx <= number_of_frames_in_interval:
-            print(f"Processing {frame_idx} frame")
-            success, img = cap.read()
-            if not success: break
+    # To improve performance, optionally mark the image as not writeable to pass by reference.
+    img.flags.writeable = False
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # frame to RGB for the face-mesh model
+    gazee = gaze_estimator.get_gaze_direction(img)
 
-            # To improve performance, optionally mark the image as not writeable to pass by reference.
-            img.flags.writeable = False
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # frame to RGB for the face-mesh model
-            gaze = gaze_estimator.get_gaze_direction(img)
-            if gaze is not None:
-                gaze_text.append(gaze_estimator.get_gaze_direction(img))
+    face_expr = face_visible_expressions.get_face_expression(img)
+    if face_expr is not None:
+        faceexpr_text += face_expr
 
-            body_mov = mediapipe_holistic.get_body_movement(img)
+    if gazee is not None:
+        p1_left = gazee[0]
+        p1_right = gazee[1]
+        p2 = gazee[2]
+        gaze_text = gazee[3]
 
-            if body_mov in ["up", "down", "right", "left"]:
-                if body_mov == "left" or body_mov == "right":
-                    if len(head_shake_idxs) > 0:
-                        if head_shake_idxs[-1] + 10 < frame_idx:
-                            if head_shake_dir[-1] != body_mov:
-                                head_shake_idxs.append(frame_idx)
-                                head_shake_dir.append(body_mov)
-                        else:
-                            head_shake_idxs.append(frame_idx)
-                    else:
-                        head_shake_idxs.append(frame_idx)
-                        head_shake_dir.append(body_mov)
-
-                elif body_mov == "up" or body_mov == "down":
-                    if len(head_nod_idxs) > 0:
-                        if head_nod_idxs[-1] + 10 < frame_idx:
-                            if head_nod_dir[-1] != body_mov:
-                                head_nod_idxs.append(frame_idx)
-                                head_nod_dir.append(body_mov)
-                        else:
-                            head_nod_idxs.append(frame_idx)
-                    else:
-                        head_nod_idxs.append(frame_idx)
-                        head_nod_dir.append(body_mov)
-
-            else:
-                mediapipe_text.append(body_mov)
-
-            face_expr = face_visible_expressions.get_face_expression(img)
-            if face_expr is not None:
-                faceexpr_text.append(face_expr)
-
-            frame_idx += 1
-
-        else:
-            gaze_counter = Counter(gaze_text)
-            mediapipe_counter = Counter(mediapipe_text)
-            face_expr_counter = Counter(faceexpr_text)
-
-            head_nod_dir_len = len(head_nod_dir)
-            head_shake_dir_len = len(head_shake_dir)
-            if head_shake_dir_len // 3 > mediapipe_counter.most_common(1)[0][1] and head_shake_dir_len // 3 > head_nod_dir_len // 3:
-                most_common_mediapipe = "HEAD SHAKE"
-            if head_nod_dir_len // 3 > mediapipe_counter.most_common(1)[0][1] and head_nod_dir_len // 3 > head_shake_dir_len // 3:
-                most_common_mediapipe = "HEAD NOD"
-            else:
-                most_common_mediapipe = mediapipe_counter.most_common(1)[0][0]
-
-            most_common_gaze = gaze_counter.most_common(1)[0][0]
-            most_common_face_expr = face_expr_counter.most_common(1)[0][0]
-
-            final_text = f"{most_common_gaze}, {most_common_mediapipe}, {most_common_face_expr}"
-
-            new_data.append("    " + f"intervals [{interval_idx}]" + ":\n")
-            new_data.append("            " + f"xmin = {x_min}" + "\n")
-            new_data.append("            " + f"xmax = {x_max}" + "\n")
-            new_data.append("            " + f"text = {final_text}" + "\n")
-            x_min += 3
-            x_max += 3
-            final_text = ""
-            frame_idx = 0
-            interval_idx += 1
-
+        cv2.line(img, p1_left, p2, (0, 0, 255), 2)
+        cv2.line(img, p1_right, p2, (0, 0, 255), 2)
     else:
-        cap.release()
-        cv2.destroyAllWindows()
+        gaze_text = ""
+
+    final_text = f"{gaze_text}, {faceexpr_text}"
+    cv2.putText(img, final_text, (20, 70), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 0), 2)
+    cv2.imshow('Recording...', img)
+    out.write(img)
+    final_text = ""
+
 
     if cv2.waitKey(5) & 0xFF == 27:
         break
 
 cap.release()
+out.release()
 cv2.destroyAllWindows()
-
-with open('face_pipeline_output.TextGrid', 'w') as file:
-    file.writelines(new_data)
+# ffmpeg -i /home/nana/Desktop/face_emotions_pipeline/output.avi -c:v copy -c:a copy -y /home/nana/Desktop/face_emotions_pipeline/output.mp4
